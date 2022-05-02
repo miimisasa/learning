17e2851dd9bd5b2f760acba90f1ffa6635defcd1
miimisasa <feliamiisa@gmail.com>
2022-05-02 22:46:44 +0300

    Create Road.java

---
+++ src/main/java/felia/lessons/LessonGBH8/Road.java
@@ -0,0 +1,13 @@
+package felia.lessons.LessonGBH8;
+
+public class Road {
+    private int lenght;
+
+    public Road(int lenght) {
+        this.lenght = lenght;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+}
