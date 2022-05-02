cbaf6f7dac71c167e068c79642420675f2d12a3d
miimisasa <feliamiisa@gmail.com>
2022-05-02 22:46:57 +0300

    Create Wall.java

---
+++ src/main/java/felia/lessons/LessonGBH8/Wall.java
@@ -0,0 +1,13 @@
+package felia.lessons.LessonGBH8;
+
+public class Wall {
+    private int height;
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
