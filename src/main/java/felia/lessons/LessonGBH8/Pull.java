3f9a3fc243830bd93e06711c7804bc23a2d59786
miimisasa <feliamiisa@gmail.com>
2022-05-05 00:31:47 +0300

    Create Pull.java

---
+++ src/main/java/felia/lessons/LessonGBH8/Pull.java
@@ -0,0 +1,13 @@
+package felia.lessons.LessonGBH8;
+
+public class Pull {
+    private int lenghtSwim;
+
+    public Pull(int lenghtSwim) {
+        this.lenghtSwim = lenghtSwim;
+    }
+
+    public int getLenghtSwim() {
+        return lenghtSwim;
+    }
+}
