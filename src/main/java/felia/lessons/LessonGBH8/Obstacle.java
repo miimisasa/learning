a7726c620d025f327dcf816cf0ec4199303ce063
miimisasa <feliamiisa@gmail.com>
2022-05-05 00:31:40 +0300

    Update Obstacle.java

--- src/main/java/felia/lessons/LessonGBH8/Obstacle.java
+++ src/main/java/felia/lessons/LessonGBH8/Obstacle.java
@@ -3,10 +3,12 @@ package felia.lessons.LessonGBH8;
 public class Obstacle {
     private Wall wall;
     private Road road;
+    private Pull pull;
 
-    protected Obstacle(Wall wall, Road road) {
+    protected Obstacle(Wall wall, Road road, Pull pull) {
         this.road = road;
         this.wall = wall;
+        this.pull = pull;
     }
 
     public int getRoadLength() {
@@ -16,5 +18,11 @@ public class Obstacle {
     public int getWallHeight() {
         return wall.getHeight();
     }
+
+    public int getPullLenght(){
+        return pull.getLenghtSwim();
+    }
+
+
 
 }
