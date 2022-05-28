2d94659f213ec115923bd60dd77004a94fd5eb43
miimisasa <feliamiisa@gmail.com>
2022-05-28 19:40:41 +0300

    Create CheckOfNumbers.java

---
+++ src/main/java/felia/lessons/TicTacToeToOOP/actions/CheckOfNumbers.java
@@ -0,0 +1,25 @@
+package felia.lessons.TicTacToeToOOP.actions;
+
+import felia.lessons.TicTacToeToOOP.Map;
+
+public class CheckOfNumbers extends Map {
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean cellIsEmpty(int x, int y) {
+        if(map[y][x] == DOT_EMPTY) {
+            return false;
+        } else {
+            System.out.println("ЭТА ЯЧЕЙКА УЖЕ ЗАНЯТА!");
+            return true;
+        }
+    }
+}
